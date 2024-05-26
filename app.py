144ee43f55d2f069f62520c0ab8fb8acb6d196b5
2oil <154411298+2oil@users.noreply.github.com>
2024-05-27 00:20:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,42 @@
-from flask import Flask, request, jsonify, url_for, render_template
+from flask import Flask, request, jsonify, render_template
 import tensorflow as tf
-import uuid
-import os
-from tensorflow.keras.models import load_model
 import numpy as np
-from PIL import Image
-import base64
-from io import StringIO
-from io import BytesIO
+from tensorflow.keras.models import load_model
 import cv2
 import pandas as pd
+import base64
 
 app = Flask(__name__)
-model = load_model('EMNIST-Balanced-Model.h5',compile=True)
+model = load_model('NewJeans2.h5', compile=True)
 ascii_map = pd.read_csv("mapping.csv")
-
 
 @app.route('/')
 def index():
     return render_template("main.html")
 
-
-@app.route('/predict',methods=["POST"])
+@app.route('/predict', methods=["POST"])
 def get_image():
     canvasdata = request.form['canvasimg']
-    # print(canvasdata)
-    encoded_data = request.form['canvasimg'].split(',')[1]
+    encoded_data = canvasdata.split(',')[1]
     nparr = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    print(img.shape)
 
     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray_image = cv2.resize(gray_image, (28, 28), interpolation=cv2.INTER_LINEAR)
-    gray_image = gray_image/255.0
+    gray_image = gray_image / 255.0
     
     gray_image = np.expand_dims(gray_image, axis=-1)
+    # 역transpose 추가
+    gray_image = np.transpose(gray_image, (1, 0, 2))
+    
     img = np.expand_dims(gray_image, axis=0)
 
     print('Image received: {}'.format(img.shape))
     prediction = model.predict(img)
     cl = list(prediction[0])
-    print("Prediction : ",ascii_map["Character"][cl.index(max(cl))])
+    predicted_char = ascii_map["Character"][cl.index(max(cl))]
 
-    ## INITIAL TF VERSION -> 2.3.0
-    # print(prediction)
-
-    return render_template("main.html", value=ascii_map["Character"][cl.index(max(cl))])
-
-
-
-
+    return jsonify(value=predicted_char)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-            
+    app.run(debug=True, port=5001)
